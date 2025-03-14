5709870d07176651409949371071b3ade9913880
mizu2727 <m98340707@gmail.com>
2025-03-15 01:27:21 +0900

    エクセルの分奏を読み込む機能を実装済み。 下記内容をコミット済み。 ■スクリプト 　▲変更  ・SignController
    　▲新規
     ・TreagerousZoneSentence
     ・SignMessageEntity
    
    ■エクセル
    ・TreageroussZoneSentence.xlsx
    パス:Assets/OriginalExcels/

---
+++ Assets/OriginalScripts/MainScene/Other/TreagerousZoneSentence.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset]
+public class TreagerousZoneSentence : ScriptableObject
+{
+	public List<SignMessageEntity> signMessage; 
+}
